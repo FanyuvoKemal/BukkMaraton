54a00cdf63ac5256b9345dec723feaa9145c94a3
FanyuvoKemal <nohitlernoparty@gmail.com>
2022-12-19 12:21:21 +0100

    kzs – 6. feladat

--- src/Main.java
+++ src/Main.java
@@ -52,5 +52,39 @@ public class Main {
         }
 
         System.out.printf("A rövidtávú versenyen %d női versenyző indult.%n%n", femaleCount);
+
+
+        System.out.println("6. feladat:");
+        boolean over6hours = false;
+
+        try {
+            Scanner scanner = new Scanner(new File("bukkm2019.txt"));
+            scanner.nextLine();
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                if (line.contains(";")){
+                    String[] parts = line.split(";");
+                    if (!parts[4].equals("")){
+                        String[] timeParts = parts[4].split(":");
+                        int hours = Integer.parseInt(timeParts[0]);
+                        if (hours >= 6){
+                            over6hours = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("A bukkm2019.txt fájl nem található!");
+        }
+
+        if (over6hours){
+            System.out.println("Volt ilyen versenyző.");
+        }
+        else {
+            System.out.println("Nem volt ilyen versenyző.");
+        }
+        System.out.println();
     }
 }
