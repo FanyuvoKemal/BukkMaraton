ec36584ee28cb02a41604057ba6cd6961e365ec1
FanyuvoKemal <nohitlernoparty@gmail.com>
2022-12-19 12:19:36 +0100

    kzs – 5. feladat

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,26 @@ public class Main {
 
         System.out.printf("A versenyzők %.2f százaléka nem teljesítette a versenyt.%n%n", 100 * (float)failedCount / (completedCount + failedCount));
 
+        System.out.println("5. feladat:");
+        int femaleCount = 0;
 
+        try {
+            Scanner scanner = new Scanner(new File("bukkm2019.txt"));
+            scanner.nextLine();
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                if (line.contains(";")){
+                    String[] parts = line.split(";");
+                    if (parts[1].endsWith("n") && parts[0].startsWith("M")){
+                        femaleCount++;
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("A bukkm2019.txt fájl nem található!");
+        }
+
+        System.out.printf("A rövidtávú versenyen %d női versenyző indult.%n%n", femaleCount);
     }
 }
